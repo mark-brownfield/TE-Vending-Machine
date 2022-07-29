c4e339266c7784f023116d6e750a6d31ad296ef5
Mark Brownfield <mbrownfield98@gmail.com>
2022-07-29 11:06:01 -0400

    push mark

--- src/main/java/com/techelevator/items/Candy.java
+++ src/main/java/com/techelevator/items/Candy.java
@@ -3,6 +3,7 @@ package com.techelevator.items;
 import java.math.BigDecimal;
 
 public class Candy extends Item{
+    //String type = "candy";
     public Candy(String slot, String itemName, BigDecimal price, int itemCount) {
         super(slot, itemName, price, itemCount);
     }
