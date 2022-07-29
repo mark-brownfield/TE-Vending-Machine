4c50c4737f03fc5d2b298ebcf0371502eed46899
Mark Brownfield <mbrownfield98@gmail.com>
2022-07-29 10:59:42 -0400

    progress push

--- src/main/java/com/techelevator/application/VendingMachine.java
+++ src/main/java/com/techelevator/application/VendingMachine.java
@@ -18,6 +18,67 @@ public class VendingMachine {
     List<Item> itemList = new ArrayList<>();
 
     public void run() {
+
+        readFile();
+
+        while(true) {
+            UserOutput.displayHomeScreen();
+            String choice = UserInput.getHomeScreenOption();
+            System.out.println(choice);
+            if (choice.equals("display")) {
+                UserOutput.displayItemList(itemList);
+            } else if (choice.equals("purchase")) {
+                 purchaseMenu();
+                }
+            else if (choice.equals("exit")) {
+                    // good bye
+                    break;
+                }
+            }
+
+
+    }
+    public void purchaseMenu(){
+        BigDecimal currentMoneyProvided = new BigDecimal(0.00);
+        while (true) {
+            UserOutput.displayPurchaseScreen();
+            String purchaseChoice = UserInput.getPurchaseScreenOption(currentMoneyProvided);
+            System.out.println(purchaseChoice);
+            if (purchaseChoice.equals("Money")) {
+                currentMoneyProvided = UserInput.getFeedMoney(currentMoneyProvided);
+            } else if (purchaseChoice.equals("Select")) {
+                UserOutput.displayItemList(itemList);
+                selectOption();
+            }
+        }
+    }
+
+
+    public void selectOption(){
+        String option = UserInput.getSelectItem(itemList);
+        System.out.println(option);
+        for (Item item : itemList) {
+            if (item.getSlot().equals(option)) {
+                String selectedOption = item.getSlot();
+                System.out.println(item.getItemName() + " " + item.getPrice() +
+                        "  count: " + item.getItemCount());
+
+                break;
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
+
+  /*  public void dispenseMessage(){
+        if (getClass(item))
+    }*/
+
+    public void despenseItem(){
+
+    }
+
+    public void readFile(){
         try (Scanner fileScanner = new Scanner(dataFile)){
             while (fileScanner.hasNextLine()) {
                 String line = fileScanner.nextLine();
@@ -43,34 +104,5 @@ public class VendingMachine {
         } catch (FileNotFoundException e){
             System.out.println("File not found");
         }
-
-        while(true) {
-            UserOutput.displayHomeScreen();
-            String choice = UserInput.getHomeScreenOption();
-            System.out.println(choice);
-            if (choice.equals("display")) {
-                UserOutput.displayItemList(itemList);
-            } else if (choice.equals("purchase")) {
-                BigDecimal currentMoneyProvided = new BigDecimal(0.00);
-                while (true) {
-                    UserOutput.displayPurchaseScreen();
-                    String purchaseChoice = UserInput.getPurchaseScreenOption(currentMoneyProvided);
-                    System.out.println(purchaseChoice);
-                    if (purchaseChoice.equals("Money")){
-                        currentMoneyProvided = UserInput.getFeedMoney(currentMoneyProvided);
-                    } else if (purchaseChoice.equals("Select")){
-                        UserOutput.displayItemList(itemList);
-                        String option = UserInput.getSelectItem(itemList);
-                        
-                    }
-                }
-                }
-            else if (choice.equals("exit")) {
-                    // good bye
-                    break;
-                }
-            }
-
-
     }
 }
